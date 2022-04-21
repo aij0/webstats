04fa1b2b7f05434ec74cf78668009b6333c20c70
aij0 <aij0@github>
2022-04-21 20:43:38 +0300

    Add website page parsing and regex matching
    Also modify test.json to support local
    test environment for debuggin

--- website_poller.py
+++ website_poller.py
@@ -1,9 +1,32 @@
+from queue import Empty
 import urllib
 from urllib.request import urlopen
+import logging
+import re
+
+def get_page(address):
+    page_raw = urllib.request.urlopen(address)
+    html_bytes = page_raw.read()
+    html = html_bytes.decode('utf-8')
+    return html
+
+def check_regex(page, regex):
+    pattern = re.compile(regex)
+    res = pattern.findall(page)
+    logging.debug(res)
+    if res:
+        return True
+    else:
+        return False
 
 
 def connect_to_servers(data):
-    uri_prefix = "https://"
+    uri_prefix = "http://"
     for server in data['servers']:
-        page = urllib.request.urlopen(uri_prefix + server["address"])
-        print(page)
+        try:
+            page = get_page(uri_prefix + server["address"])
+            content_found = check_regex(page, server["regex"])
+            logging.debug(content_found)
+        except Exception as err:
+            logging.error("Error: %s", err)
+        
