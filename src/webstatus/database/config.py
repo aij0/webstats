7f5a65ca5c3a1708ad037b5a22793fb1707b8b2b
aij0 <aij0@github>
2022-04-23 21:16:09 +0300

    Database: Add config parser
    This module parsers and validates user given
    configuration about the database access

---
+++ src/webstatus/database/config.py
@@ -0,0 +1,33 @@
+import json
+import logging
+import sys
+from common.validators import validate_schema_details
+
+databaseSchema = {
+        "type": "object",
+        "properties": {
+            "server": {"type": "string"},
+            "address": {"type": "string", "format": "ip-address"},
+            "port": {"type": "number"},
+            "user": {"type": "string"},
+            "password": {"type": "number"}
+        }
+    }
+
+
+def parse_config(file):
+    data = ""
+    try:
+        file = open(file)
+        data = json.load(file)
+        if validate_schema_details(data, databaseSchema):
+            logging.debug("database-server details ok")
+        else:
+            logging.warning("Issue with database-server detail, \
+                            ´please check")
+        file.close()
+    except FileNotFoundError:
+        logging.error("File not found")
+        sys.exit(2)
+
+    return data
